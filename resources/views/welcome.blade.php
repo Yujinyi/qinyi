682c2383988d638024671d702e967db96a154e0e
root <851207685@qq.com>
2017-07-12 12:09:06 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Life is a pure flame,and we live by an invisible sun within us.  </div>
+		<div class="context">----Jingtao is waiting for you!----</div>
+            </div>
+        </div>
+    </body>
+</html>
+
+<script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAcYCDti4EXkqdLSDiqKPJIriqKMox2zFw",
+    authDomain: "hexo-5b04d.firebaseapp.com",
+    databaseURL: "https://hexo-5b04d.firebaseio.com",
+    projectId: "hexo-5b04d",
+    storageBucket: "hexo-5b04d.appspot.com",
+    messagingSenderId: "113214474860"
+  };
+  firebase.initializeApp(config);
+</script>
